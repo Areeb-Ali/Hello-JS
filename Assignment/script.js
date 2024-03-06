f776396bb0c2081778cbfa3566a25694ef1e875a
Areeb-Ali <areebithink@gmail.com>
2024-03-06 17:57:50 +0500

    Completed Structure

---
+++ Assignment/script.js
@@ -0,0 +1,22 @@
+// var chemistoryMarks = document.querySelector("#che-marks");
+// var mathMarks = document.querySelector("math-marks");
+// var physicsMarks = document.querySelector("phy-marks");
+// var computerMarks = document.querySelector("com-marks");
+// var percentage = document.querySelector("#percentage");
+// var obtainedMarks = document.querySelector("total-marks-obtained");
+
+// function calulation() {
+//   console.log(chemistoryMarks.value);
+//   console.log(mathMarks.value);
+//   console.log(physicsMarks.value);
+//   console.log(computerMarks.value);
+
+//   var totalMarks = 400;
+//   var obtainedMarks =
+//     chemistoryMarks.value +
+//     mathMarks.value +
+//     physicsMarks.value +
+//     computerMarks.value;
+//   console.log(obtainedMarks);
+//   obtainedMarks.innerHTML = obtainedMarks;
+// }
