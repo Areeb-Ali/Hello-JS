48954d363059908cd73f17f3daeb5df8bbcdadd1
Areeb-Ali <areebithink@gmail.com>
2024-03-08 17:56:59 +0500

    Pratice for Variables

---
+++ Variable Zoo/script.js
@@ -0,0 +1,17 @@
+let animalName = "Cat";
+let age = "1";
+let personality = "Sweet, gentle, and affectionate";
+let breeds = "Persian";
+let weight = "7 to 12 pounds";
+let lenght  = "14 to 17 inches"
+let lifeSpan = "15 to 20 years";
+let origin = "Iran & Persia";
+
+console.log("Animal Name:", animalName);
+console.log("Age", age, "year old");
+console.log("Breed is", breeds);
+console.log("Personality is", personality);
+console.log("Average Weight is", weight);
+console.log("Average Lenght is", lenght);
+console.log("Average Lifespan is ", lifeSpan);
+console.log("Origin of Persian Cat is", origin);
