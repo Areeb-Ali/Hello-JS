609775e4f50c030a0b0c20c783d30b640426d0f7
Areeb-Ali <areebithink@gmail.com>
2024-03-09 15:20:53 +0500

    Favourite things variables project

---
+++ Variable Pratice/Favorite Things/script.js
@@ -0,0 +1,13 @@
+let favoriteName = "Hazrat Muhammad sallallahu alaihi wasallam";
+let favoriteFood = "burgers";
+let favoriteSport = "Cricket";
+let favoriteCar = "Porsche 911 GT3";
+let favoritePlace = "Madina";
+let hobby = "Cricket, Coding, Praying, Gaming, and also overthinking"
+
+console.log ("My favorite Name is ",favoriteName);
+console.log ("My favorite food is ",favoriteFood);
+console.log ("My favorite sport is ",favoriteSport);
+console.log ("My dream car is ",favoriteCar);
+console.log ("My favorite place where i found peace is ",favoritePlace);
+console.log ("My daily routine be like ",hobby);
